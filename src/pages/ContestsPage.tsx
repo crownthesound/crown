4971fdd6158171eb32b421ae18d699696ff1cc48
crownthesound <info@crownthesound.com>
2025-07-08 11:18:19 -0700

    Change 'View Details' to 'Leaderboard' on contests page

--- src/pages/ContestsPage.tsx
+++ src/pages/ContestsPage.tsx
@@ -477,7 +477,7 @@ export function ContestsPage() {
                       to={`/l/${contest.id}`}
                       className="flex-1 px-4 py-2 bg-white/10 hover:bg-white/20 text-white rounded-lg transition-colors text-center text-sm font-medium"
                     >
-                      View Details
+                      Leaderboard
                     </Link>
                     {session && userSubmissions[contest.id] ? (
                       <button
